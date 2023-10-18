be0948af7a63d5b1f2fa6992ee08386d2c1d8e30
Gori Franco <gorifranco@gmail.com>
2023-10-18 20:45:26 +0200

    Objectes predefinits acabat

--- Objectes_Predefinits/funcions.js
+++ Objectes_Predefinits/funcions.js
@@ -1,14 +1,15 @@
-function maxim(array){
+function maxim(array) {
     let max = array[0];
     for (let max1 of array) {
-        if(max1 > max) max = max1;
+        if (max1 > max) max = max1;
     }
     return max;
 }
-function minim(array){
+
+function minim(array) {
     let min = array[0];
     for (let min1 of array) {
-        if(min1 < min) min = min1;
+        if (min1 < min) min = min1;
     }
     return min;
 }
@@ -18,5 +19,54 @@ function mitjana(array) {
     for (let arrayElement of array) {
         total += parseInt(arrayElement);
     }
-    return total/array.length;
+    return total / array.length;
+}
+
+function total(array) {
+    let total = 0;
+    for (let arrayElement of array) {
+        total += parseInt(arrayElement);
+    }
+    return total;
+}
+
+function copiarArray(array) {
+    let copiaArray = [];
+    array.copyTo(copiaArray);
+    return copiaArray;
+}
+
+function moda(array) {
+    let valors = [];
+    let repeticions = [];
+
+    for (let arrayElement of array) {
+        if (valors.includes(array)) {
+            repeticions[valors.findIndex(arrayElement)]++
+        } else {
+            valors.push(arrayElement);
+            repeticions.push(1);
+        }
+    }
+    return valors[repeticions.findIndex(maxim(repeticions))];
+}
+
+function toCamelCase(frase) {
+    let copia = "";
+    for (let i = 0; i < frase.length; i++) {
+        if (frase.charAt(i) === ' ') {
+            frase.charAt(i + 1).toUpperCase();
+        } else {
+            copia.concat(frase.charAt(i));
+        }
+    }
+    return copia;
+}
+
+function llevarDuplicats(array) {
+    let temp = [];
+    for (let arrayElement of array) {
+        if (!temp.includes(arrayElement)) temp.push(arrayElement);
+    }
+    return temp;
 }
