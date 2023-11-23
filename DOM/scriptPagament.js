25e54ee9fa511813ec1e88757876caef48de14b2
Gori Franco <gorifranco@gmail.com>
2023-11-23 17:46:28 +0100

    acabat DOM

--- DOM/scriptPagament.js
+++ DOM/scriptPagament.js
@@ -1,7 +1,13 @@
-function crearFormulariPagament() {
+function crearFormulariTargeta() {
+    let down = false;
+
     let divExercici = document.querySelector("fieldset").nextElementSibling
-    console.log(divExercici.name);
     divExercici.hidden = ""
+
+    while(divExercici.firstElementChild !== null){
+        console.log(divExercici.firstElementChild);
+        divExercici.removeChild(divExercici.firstElementChild)
+    }
 
     let divNombre = document.createElement("div")
     divNombre.classList.add("col-md-8")
@@ -18,11 +24,14 @@ function crearFormulariPagament() {
     inputNombre.maxLength = 19
 
     inputNombre.addEventListener("keypress", function (evt) {
+        if (down) evt.preventDefault()
+        down = true
         if (!evt.key.match("[1234567890]|Backspace")) {
             evt.preventDefault()
         }
     })
     inputNombre.addEventListener("keyup", function (evt) {
+        down = false
         if (evt.key === "Backspace" && (inputNombre.value.length === 4 || inputNombre.value.length === 8 || inputNombre.value.length === 9 || inputNombre.value.length === 14)) {
             inputNombre.value = inputNombre.value.substring(0, inputNombre.value.length - 1)
         }
@@ -48,13 +57,16 @@ function crearFormulariPagament() {
     inputData.id = "inputData"
     inputData.type = "text"
     inputData.maxLength = 5
-    inputData.addEventListener("keydown", function (evt) {
+    inputData.addEventListener("keypress", function (evt) {
+        if (down) evt.preventDefault()
+        down = true
         console.log(evt.key);
         if (!evt.key.match("[1234567890]|Backspace")) {
             evt.preventDefault()
         }
     })
     inputData.addEventListener("keyup", function (evt) {
+        down = false
         if (evt.key === "Backspace" && inputData.value.length === 2) {
             inputData.value = inputData.value.charAt(0)
         }
@@ -93,4 +105,35 @@ function crearFormulariPagament() {
     divExercici.appendChild(divNombre)
     divExercici.appendChild(divData)
     divExercici.appendChild(divCVV)
+}
+
+function crearFormulariTransferencia(){
+
+    let divExercici = document.querySelector("fieldset").nextElementSibling
+    divExercici.hidden = ""
+    while(divExercici.firstElementChild !== null){
+        console.log(divExercici.firstElementChild);
+        divExercici.removeChild(divExercici.firstElementChild)
+    }
+
+    let div1 = document.createElement("div")
+    div1.classList.add("row")
+    div1.classList.add("mt-2")
+
+    let span = document.createElement("span");
+    span.appendChild(document.createTextNode("Conta bancària: "))
+    span.style.fontWeight = "bold";
+
+    let pCC = document.createElement("p");
+    pCC.appendChild(span); // Append the span element after setting its text content
+    pCC.appendChild(document.createTextNode("ES6000491500051234567892"))
+    div1.appendChild(pCC);
+
+
+    let pMissatge = document.createElement("p")
+    pMissatge.textContent = "En rebre la transferència li enviarem un correu amb la confirmació de la comanda."
+
+    div1.appendChild(pMissatge)
+    divExercici.appendChild(div1)
+
 }
